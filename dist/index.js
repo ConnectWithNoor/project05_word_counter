aff930fcd49c0a5d39f37dc2d8f9310ed0a33076
Noor Muhammad <connectwithnoor1@gmail.com>
2022-12-25 17:09:08 +0500

    finish

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,9 @@
 import inquirer from 'inquirer';
-
-const answer: { sentence: string } = await inquirer.prompt({
-  type: 'input',
-  message: 'Enter a sentence: ',
-  name: 'sentence',
+const answer = await inquirer.prompt({
+    type: 'input',
+    message: 'Enter a sentence: ',
+    name: 'sentence',
 });
-
 const { sentence } = answer;
 const count = sentence.trim().split(' ').length;
 console.log(`THE WORD COUNT FOR YOUR SENTENCE IS: ${count}`);
